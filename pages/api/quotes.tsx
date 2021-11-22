91d06d38ec45fb802acddc093d0cd29ee8bd168c
Brian Stanley <brian@MacBook-Pro-de-Brian.local>
2021-11-21 21:30:11 -0300

    some changes in provider

--- pages/api/quotes.tsx
+++ pages/api/quotes.tsx
@@ -25,11 +25,19 @@ handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
         results[Provider.DOLAR_HOY] = {provider: Provider.DOLAR_HOY, error: e.error}
     }
 
+    try {
+        let data = await fetchRates(Provider.AMBITO);
+        console.log(data)
+        results[Provider.AMBITO] = await fetchRates(Provider.AMBITO);
+    } catch (e) {
+        console.log('entra por el error', e)
+        results[Provider.AMBITO] = {provider: Provider.AMBITO, error: e.error}
+    }
+
     const responseData = [
         {
-            "buy_price": 140.3,
-            "sell_price": 144,
-            "source": "https://www.ambito.com/contenidos/dolar.html"
+            ...results[Provider.AMBITO],
+            source: Provider.AMBITO
         },
         {
             ...results[Provider.DOLAR_HOY],
@@ -38,7 +46,7 @@ handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
         {
             "buy_price": 140.3,
             "sell_price": 144,
-            "source": "https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB"
+            "source": Provider.CRONISTA
         }
     ];
     res.json(responseData);
@@ -53,7 +61,9 @@ enum Provider {
 
 const ProviderConfig = {
     [Provider.AMBITO]: {
-        url: 'https://www.ambito.com/contenidos/dolar.html'
+        url: 'https://www.ambito.com/contenidos/dolar.html',
+        parent_selector: "div[data-indice='/dolar/informal']",
+        data_selector: '/dolar/informal'
     },
     [Provider.DOLAR_HOY]: {
         url: "https://dolarhoy.com/",
@@ -73,21 +83,38 @@ interface IQuotes {
     sell_price: number
 }
 
+async function fetchHtml(url: string) {
+    const response = await fetch(url);
+    const htmlString = await response.text();
+    return cheerio.load(htmlString);
+}
+
 async function fetchRates(provider: Provider): Promise<IQuotes> {
     let quotes: IQuotes = {buy_price: 0, sell_price: 0};
+    let config;
+    let $;
      switch (provider) {
          case Provider.DOLAR_HOY:
-            let config = ProviderConfig[Provider.DOLAR_HOY];
-            const response = await fetch(config.url);
-            const htmlString = await response.text();
-            const $ = cheerio.load(htmlString);
+            config = ProviderConfig[Provider.DOLAR_HOY];
+            $ = await fetchHtml(config.url)
             quotes.buy_price = $(config.parent_selector).find(config.value_container.buy).text().replace('$', '');
             quotes.sell_price = $(config.parent_selector).find(config.value_container.sell).text().replace('$', '');
             break;
         case Provider.CRONISTA:
             break;
-        case Provider.AMBITO:
-            break;
+         case Provider.AMBITO:
+             // @TODO change to Puppeter for wait xhr to fully loaded
+            // config = ProviderConfig[Provider.AMBITO];
+            // $ = await fetchHtml(config.url)
+            //  let data= $(config.parent_selector);
+            // console.log('DATA: ', data.first().text());
+            // quotes.buy_price = $(config.parent_selector)
+            //     .find('.value .data-compra')
+            //     .text();
+            // quotes.sell_price = $(config.parent_selector)
+            //     .find('.value .data-venta')
+            //     .text();
+            // break;
         default:
             break;
     }
