1fa8399ebdecbcab434a0f9bff722fc730eba19a
Brian Stanley <brian@MacBook-Pro-de-Brian.local>
2021-11-22 13:30:40 -0300

    add loading component

--- components/EndpointResultViewer/index.js
+++ components/EndpointResultViewer/index.js
@@ -1,10 +1,10 @@
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import {useGet} from "../../useRequest";
+import Loading from "../shared/loading";
 
 const EndpointResultViewer = ({path}) => {
     const {data, error } = useGet(path)
     if (error) return <h2>Oops something went wrong.</h2>;
-    if (!data) return <h2>Loading</h2>
     return (
         <div
             className="transition-all duration-150 flex max-w-md min-w-full px-2 py-6 sm:w-full lg:w-1/3"
@@ -16,15 +16,20 @@ const EndpointResultViewer = ({path}) => {
                     <div className="font-bold uppercase">
                         {path}
                     </div>
-                    <div className="text-gray-500 text-sm mt-1">Last update at {data[1]}</div>
+                    {data && <div className="text-gray-500 text-sm mt-1">Last update at {data[1]}</div>}
                 </div>
 
                 <div
                     className="p-6 bg-white border-b border-gray-200 h-3/4 break-words"
                 >
-                    <SyntaxHighlighter className="min-h-full max-h-full" language="json" >
+                    {!data &&
+                    <Loading />
+                    }
+                    {data &&
+                    <SyntaxHighlighter className="min-h-full max-h-full" language="json">
                         {JSON.stringify(data[0], undefined, 2)}
                     </SyntaxHighlighter>
+                    }
                 </div>
                 <div className="p-6 bg-white border-gray-200 text-right">
                     <a className="bg-blue-500 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase"
--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,15 @@ export default function Home() {
             </div>
             <div>
                 <div className="flex mt-10 space-x-10">
-                    <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+                    <div className="py-4 px-8 bg-white shadow-lg rounded-lg ">
                         <div className="flex justify-center md:justify-end -mt-16">
                             <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500"
                                  src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" />
                         </div>
                         <div>
-                            <h2 className="text-gray-800 text-3xl font-semibold">Design Tools</h2>
+                            <h2 className="text-gray-800 text-3xl font-semibold">About</h2>
                             <p className="mt-2 text-gray-600">
-                                Hi everyone! I built this challenge Using NextJS + React. I deployed to Vercel with serverless in order to serve the API endpoints.
+                                Hi everyone! I built this challenge Using NextJS + React. I deployed to Vercel in order to serve the API endpoints as Serverless Functions.
                                 Hope you enjoy it!.
                             </p>
                         </div>
