a39b60df3184ed7e5d590ee12cdcdf2e0d996004
Brian Stanley <brian@MacBook-Pro-de-Brian.local>
2021-11-22 11:13:13 -0300

    optimize puppeteer, change cache control

--- pages/api/quotes.tsx
+++ pages/api/quotes.tsx
@@ -1,24 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import nc from "next-connect";
 import {Browser} from "puppeteer";
-// const puppeteer = require('puppeteer');
 const handler = nc<NextApiRequest, NextApiResponse>();
-
-interface IResultError {
-    provider: Provider,
-    error: string
-}
-
 const chromium = require('chrome-aws-lambda');
-function getLastSyncTime () {
-    const currentDate = new Date();
-    const minutes = `0${currentDate.getMinutes()}`;
-    const seconds = `0${currentDate.getSeconds()}`;
-    return `${currentDate.getHours()}:${minutes.slice(-2)}:${seconds.slice(-2)}`
-}
 
 handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
-    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+    res.setHeader('Cache-Control', 's-maxage=60');
 
     let results: Results = {
         [Provider.DOLAR_HOY]: {buy_price: 0, sell_price: 0},
@@ -37,14 +24,12 @@ handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         results[Provider.DOLAR_HOY] = await fetchRates(Provider.DOLAR_HOY, browser);
     } catch (e) {
-        console.log('error ', e)
         results[Provider.DOLAR_HOY] = {provider: Provider.DOLAR_HOY, error: e.error}
     }
 
     try {
         results[Provider.AMBITO] = await fetchRates(Provider.AMBITO, browser);
     } catch (e) {
-        console.log('entra por el error', e)
         results[Provider.AMBITO] = {provider: Provider.AMBITO, error: e.error}
     }
     await browser.close();
@@ -52,37 +37,41 @@ handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
     const responseData = [
         {
             ...results[Provider.AMBITO],
-            source: Provider.AMBITO
+            source: process.env.AMBITO_SOURCE
         },
         {
             ...results[Provider.DOLAR_HOY],
-            source: Provider.DOLAR_HOY
+            source: process.env.DOLAR_HOY_SOURCE
         },
         {
             "buy_price": 140.3,
             "sell_price": 144,
-            "source": Provider.CRONISTA
+            "source": process.env.CRONISTA_SOURCE
         }
     ];
     res.json(responseData);
 })
 
 enum Provider {
-    CRONISTA = 'https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB',
-    AMBITO = 'https://www.ambito.com/contenidos/dolar.html',
-    DOLAR_HOY = 'https://dolarhoy.com/',
+    CRONISTA,
+    AMBITO,
+    DOLAR_HOY,
 }
 
 interface IProvider {
     url: string,
     buy_selector: string,
     sell_selector: string,
-    last_sync: string
 }
 
 interface IQuotes {
     buy_price: number,
-    sell_price: number
+    sell_price: number,
+}
+
+interface IResultError {
+    provider: Provider,
+    error: string
 }
 
 type Results = {
@@ -95,38 +84,47 @@ type ProviderConfig = {
 
 const ProviderConfig: ProviderConfig = {
     [Provider.AMBITO]: {
-        url: 'https://www.ambito.com/contenidos/dolar.html',
+        url: process.env.AMBITO_SOURCE,
         buy_selector: "div[data-indice='/dolar/informal'] > .align-items-end > .first > .value",
         sell_selector: "div[data-indice='/dolar/informal'] > .align-items-end > .second > .value",
-        last_sync: ""
     },
     [Provider.DOLAR_HOY]: {
-        url: "https://dolarhoy.com/",
+        url: process.env.DOLAR_HOY_SOURCE,
         buy_selector: '.tile .is-parent .is-5 > .tile > .values > .compra > .val ',
         sell_selector: '.tile .is-parent .is-5 > .tile > .values > .venta > .val ',
-        last_sync: ""
 
     },
     [Provider.CRONISTA]: {
-        url: "https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB",
+        url: process.env.CRONISTA_SOURCE,
         buy_selector: "",
         sell_selector: "",
-        last_sync: ""
     }
 }
 
 async function scrapProvider(config, browser: Browser) {
-    return await (async (config, browser: Browser, lastSync) => {
+    return await (async (config, browser: Browser) => {
         const page = await browser.newPage();
+        await page.setRequestInterception(true);
+        page.on('request', (req) => {
+            if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image'){
+                req.abort();
+            }
+            else {
+                req.continue();
+            }
+        });
+
         await page.goto(config.url);
+        await page.setRequestInterception(true)
+
         return await page.evaluate(({config, lastSync}) => {
             return {
                 last_sync: lastSync,
                 buy_price: parseFloat(document.querySelector(config.buy_selector).textContent.replace(",", ".").replace(/^(-)|[^0-9.,]+/g, '$1')),
                 sell_price: parseFloat(document.querySelector(config.sell_selector).textContent.replace(",", ".").replace(/^(-)|[^0-9.,]+/g, '$1'))
             }
-        },{config, lastSync});
-    })(config, browser, getLastSyncTime())
+        },{config});
+    })(config, browser)
 }
 
 async function fetchRates(provider: Provider, browser): Promise<IQuotes> {
