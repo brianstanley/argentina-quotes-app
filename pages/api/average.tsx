beb00855a9304948a7ba17926beca186f64799ee
Brian Stanley <brian@MacBook-Pro-de-Brian.local>
2021-11-22 22:01:19 -0300

    function refactor getAveragePrice

--- pages/api/average.tsx
+++ pages/api/average.tsx
@@ -2,18 +2,29 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import {getQuotes} from "./quotes";
 import {Quote} from "../../ts/types";
 import {IAverageResponse} from "../../ts/interfaces";
+import {providerConfig} from "../../configs";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.setHeader('Cache-Control', process.env.CACHE_CONTROL);
     const quotes: Quote[] = await getQuotes();
 
-    const sell_price: number = quotes.reduce((acc, obj) => (acc + obj.sell_price), 0) / 3;
-    const buy_price: number = quotes.reduce((acc, obj) => (acc + obj.buy_price), 0) / 3;
+    const sellPrice: number = getAveragePrice(quotes, 'sell_price')
+    const buyPrice: number =  getAveragePrice(quotes,'buy_price');
 
     const responseData: IAverageResponse = {
-        "average_buy_price": parseFloat(sell_price.toFixed(2)),
-        "average_sell_price": parseFloat(buy_price.toFixed(2)),
+        "average_buy_price": parseFloat(sellPrice.toFixed(2)),
+        "average_sell_price": parseFloat(buyPrice.toFixed(2)),
         "last_sync": new Date().toISOString().slice(0,19)
     };
     res.status(200).json(responseData)
+}
+
+function getAveragePrice(quotes, key): number {
+    let providersSucceedLength = Object.keys(providerConfig).length;
+    return quotes.reduce((acc, obj) => {
+        if (obj[key]) {
+            return acc + obj[key]
+        }
+        providersSucceedLength--;
+    }, 0) / providersSucceedLength;
 }
