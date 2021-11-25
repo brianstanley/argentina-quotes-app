a56f03040bb0a2dca5d33b2109a09d4874b5d974
Brian Stanley <brian@MacBook-Pro-de-Brian.local>
2021-11-25 09:15:29 -0300

    useValidating from swr hook

--- components/EndpointResultViewer/index.js
+++ components/EndpointResultViewer/index.js
@@ -3,23 +3,26 @@ import { useGet } from '../../useRequest'
 import Loading from '../shared/loading'
 
 const EndpointResultViewer = ({ path }) => {
-  const { data, error } = useGet(path)
+  const { data, error, isValidating, mutate } = useGet(path)
   if (error) return <h2>Oops something went wrong.</h2>
+
   return (
     <div className="transition-all duration-150 flex max-w-md min-w-full px-2 py-6 sm:w-full lg:w-1/3">
       <div className="flex min-w-full flex-col items-stretch  pb-4 mb-6 transition-all duration-150 bg-white rounded-lg shadow-lg hover:shadow-2xl">
         <div className="border-b border-gray-200 bg-white px-6 py-4">
           <div className="font-bold uppercase">{path}</div>
-          {data && (
+          {isValidating && (
+            <div className="text-gray-500 text-sm mt-1">Loading...</div>
+          )}
+          {!isValidating && (
             <div className="text-gray-500 text-sm mt-1">
               Last update at {data[1]}
             </div>
           )}
         </div>
-
         <div className="p-6 bg-white border-b border-gray-200 h-3/4 break-words">
-          {!data && <Loading />}
-          {data && (
+          {isValidating && <Loading />}
+          {!isValidating && (
             <SyntaxHighlighter
               className="min-h-full max-h-full"
               language="json"
@@ -29,12 +32,18 @@ const EndpointResultViewer = ({ path }) => {
           )}
         </div>
         <div className="p-6 bg-white border-gray-200 text-right">
-          <a
-            className="bg-blue-500 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase"
-            href="#"
+          <button
+            disabled={isValidating}
+            className={
+              'bg-blue-500 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase' +
+              (isValidating ? ' opacity-25' : '')
+            }
+            onClick={() => {
+              mutate(path)
+            }}
           >
             Refresh
-          </a>
+          </button>
         </div>
       </div>
     </div>
--- useRequest.tsx
+++ useRequest.tsx
@@ -1,27 +1,29 @@
-import useSWR from "swr"
+import useSWR from 'swr'
 
 const getLastSyncTime = () => {
-    const currentDate = new Date();
-    const minutes = `0${currentDate.getMinutes()}`;
-    const seconds = `0${currentDate.getSeconds()}`;
-    return `${currentDate.getHours()}:${minutes.slice(-2)}:${seconds.slice(-2)}`
+  const currentDate = new Date()
+  const minutes = `0${currentDate.getMinutes()}`
+  const seconds = `0${currentDate.getSeconds()}`
+  return `${currentDate.getHours()}:${minutes.slice(-2)}:${seconds.slice(-2)}`
 }
 
-const fetcher = async (url) => {
-    const res = await fetch(url)
-    const data = await res.json();
+export const fetcher = async (url) => {
+  const res = await fetch(url)
+  const data = await res.json()
 
-    return [data, getLastSyncTime()]
+  return [data, getLastSyncTime()]
 }
 
-const baseUrl = "/api";
+const baseUrl = '/api'
 
-export const useGet = path => {
-    if (!path) {
-        throw new Error("Path is required")
-    }
+export const useGet = (path) => {
+  if (!path) {
+    throw new Error('Path is required')
+  }
 
-    const url = `${baseUrl}${path}`
-    const { data, error } = useSWR(url, fetcher, { refreshInterval: 15000 });
-    return { data, error }
+  const url = `${baseUrl}${path}`
+  const { data, error, isValidating, mutate } = useSWR(url, fetcher, {
+    refreshInterval: 15000,
+  })
+  return { data, error, isValidating, mutate }
 }
